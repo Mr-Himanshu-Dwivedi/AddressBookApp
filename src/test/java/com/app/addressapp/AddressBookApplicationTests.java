eb41616c12d2c911aff10ebf3ce93e16ae5cf47e
Mr-Himanshu-Dwivedi <himanshudwivedi2003@gmail.com>
2025-03-06 20:36:05 +0530

    UC1 connect database

---
+++ src/test/java/com/app/addressapp/AddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.addressapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
